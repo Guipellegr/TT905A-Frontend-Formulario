6b81efa8b4cd9171f26bec64a299e0eb9d7b3e3e
Guipellegr <g173879@dac.unicamp.br>
2021-06-22 12:35:17 -0300

    Correção

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-let url = "https://tt905-mensagem-guilherme-lucio.herokuapp.com/festas"
+let url = "https://tt905-mensagem-guilherme-lucio.herokuapp.com/festas/"
 
 async function callFetchWithGet(){
     let headers = new Headers();
@@ -10,44 +10,10 @@ async function callFetchWithGet(){
     const output = document.getElementById("json");
     const response = await fetch(url, options);
 
-    if (response.status >= 200 && response.status <= 300);{
+    if (response.status >= 200 && response.status <= 300){
         console.log("Funcionou");
         output.innerHTML = await response.text();
-    } else{
-        console.log("Deu erro");
-    }
-}
-
-async function callFetchWithPost(festa){
-    const options = {
-        method : 'POST',
-        mode: 'cors',
-        headers: {
-            'Accep' : 'application/jason',
-            'content-type' : 'application/json',
-
-        },
-        body :JSON.stringify({
-            'festa' : festa
-        })
-    }
-    await fetch(url, options);
-}
-
-async function callFetchWithPut(){
-    let headers = new Headers();
-    const options = {
-        method : 'GET',
-        mode: 'cors',
-        headers: headers
-    }
-}
-
-async function callFetchWithDelete(){
-    let headers = new Headers();
-    const options = {
-        method : 'GET',
-        mode: 'cors',
-        headers: headers
+    } else {
+        console.log("Deu errado");
     }
 }
