8ecb9612b2d6049a5803ef127374d7ba98101a1c
Guipellegr <g173879@dac.unicamp.br>
2021-06-22 11:51:13 -0300

    Segundo commit

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+let url = "https://tt905-mensagem-guilherme-lucio.herokuapp.com/festas"
+
+async function callFetchWithGet(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json");
+    const response = await fetch(url, options);
+
+    if (response.status >= 200 && response.status <= 300);{
+        console.log("Funcionou");
+        output.innerHTML = await response.text();
+    }
+    else{
+        console.log("Deu erro");
+    }
+}
