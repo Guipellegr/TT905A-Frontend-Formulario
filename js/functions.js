1d36be53e274bb8979d9b864f43212f08db4ab12
Guipellegr <g173879@dac.unicamp.br>
2021-06-22 12:43:01 -0300

    mudanças

--- js/functions.js
+++ js/functions.js
@@ -17,3 +17,36 @@ async function callFetchWithGet(){
         console.log("Deu errado");
     }
 }
+
+async function callFetchWithPost(festa){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'festa' : festa
+        })
+    }
+    await fetch](url, options);
+}
+
+async function callFetchWithPut(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+}
+
+async function callFetchWithDelete(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+}
